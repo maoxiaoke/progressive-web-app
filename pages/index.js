1b1b76f9cf9d71b696eb8aca79b06c999c1a5648
maoxiaoke <maoxiaoke@outlook.com>
2021-05-26 21:21:56 +0800

    udpate

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,6 @@ export default function Home() {
         </h1>
 
         <div onClick={test}>test</div>
-        <div>这里加一个东东</div>
         <div>再加一个东西吧</div>
 
         <p className={styles.description}>
